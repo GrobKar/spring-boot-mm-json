c19564ebef4c3b45d88c55b3ca8e8d50d55f2583
GrobKar <44326791+GrobKar@users.noreply.github.com>
2020-06-25 17:52:12 +0200

    third commit

---
+++ src/test/java/guru/springframeword/json/springbootmmjson/SpringBootMmJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframeword.json.springbootmmjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMmJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
